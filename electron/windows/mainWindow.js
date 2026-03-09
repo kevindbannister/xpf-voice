411e1417982d473bec350ab05538d65c9d69c66b
kevindbannister <kevin@kevindbannister.co.uk>
2026-03-09 18:23:41 +0000

    Fix Electron renderer index path

--- electron/windows/mainWindow.js
+++ electron/windows/mainWindow.js
@@ -21,7 +21,8 @@ function createMainWindow() {
   });
 
   logger('Main UI opened');
-  mainWindow.loadFile(path.join(__dirname, '../../index.html'));
+  console.log('[XPROFLOW VOICE] Loading renderer UI');
+  mainWindow.loadFile(path.join(__dirname, '../../public/index.html'));
 
   mainWindow.on('closed', () => {
     mainWindow = null;
